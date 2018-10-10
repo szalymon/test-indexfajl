68de984b866fda32ed1bf24081e4f1e2507c4cb5
Simon Urbanovics <urbsimon@gmail.com>
2018-10-10 10:33:43 +0200

    Update encoding, Use of Collator

--- src/hu/urbanovics/doctusoft/indexfajl/Main.java
+++ src/hu/urbanovics/doctusoft/indexfajl/Main.java
@@ -1,8 +1,10 @@
 package hu.urbanovics.doctusoft.indexfajl;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.Collator;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -10,26 +12,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Map<String, List<Integer>> indexMap = new HashMap<>();
-
-        Scanner scanner = new Scanner(new FileReader("szoveg.txt"));
         int lineCounter = 0;
-        while (scanner.hasNextLine()) {
-            lineCounter++;
-            String line = scanner.nextLine();
-            StringTokenizer tokenizer = new StringTokenizer(line, " ");
-            while (tokenizer.hasMoreTokens()) {
-                final String token = tokenizer.nextToken().toLowerCase();
-                if (!indexMap.containsKey(token)) {
-                    indexMap.put(token, new ArrayList<>());
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("szoveg.txt"), StandardCharsets.ISO_8859_1)) {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                lineCounter++;
+                StringTokenizer tokenizer = new StringTokenizer(line, " ");
+                while (tokenizer.hasMoreTokens()) {
+                    final String token = tokenizer.nextToken().toLowerCase();
+                    if (!indexMap.containsKey(token)) {
+                        indexMap.put(token, new ArrayList<>());
+                    }
+                    indexMap.get(token).add(lineCounter);
                 }
-                indexMap.get(token).add(lineCounter);
             }
         }
-
         ArrayList<Map.Entry<String, List<Integer>>> entries = new ArrayList<>(indexMap.entrySet());
-        entries.sort(Comparator.comparing(Map.Entry::getKey));
 
-        try (FileWriter fileWriter = new FileWriter("szoveg-index.txt")) {
+        Collator collator = Collator.getInstance();
+//        entries.sort(Comparator.comparing(Map.Entry::getKey));
+        entries.sort((a, b) -> collator.compare(a.getKey(), b.getKey()));
+
+        try (BufferedWriter fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("szoveg-index.txt"), StandardCharsets.ISO_8859_1))) {
             entries.forEach(item -> {
                 try {
                     String indexes = String.join(",", item.getValue().stream().map(Object::toString).collect(Collectors.toList()));
